9d03bbcf3befce302c3a0454822f8636580c5b13
Marius Guggenmos <MariusGuggenmos@gmail.com>
2021-11-16 18:54:22 +0100

    Non-prod commit, just for running the Ghidra tests
    This commit should be reverted/removed if this ever gets merged

--- anvill/python/anvill/__init__.py
+++ anvill/python/anvill/__init__.py
@@ -16,13 +16,17 @@
 from .arch import *
 import os
 
-try:
-    import ida_idp
-    from .ida import *
-except ImportError as e:
-    try:
-        import binaryninja
-        from .binja import *
+if True:
+    from .ghidra3 import *
+else:
 
+    try:
+        import ida_idp
+        from .ida import *
     except ImportError as e:
-        raise NotImplementedError("Could not find either IDA or Binary Ninja APIs")
+        try:
+            import binaryninja
+            from .binja import *
+
+        except ImportError as e:
+            raise NotImplementedError("Could not find either IDA or Binary Ninja APIs")
